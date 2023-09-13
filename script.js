426de9a3be93d5c0b7ab3f432d02b2da45bf1600
dwqdqdq <80990185+dwqdqdq@users.noreply.github.com>
2023-09-12 21:36:27 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+window.addEventListener("DOMContentLoaded", function () {
+    // Replace 'YOUR_URL_HERE' with the URL of the website you want to embed
+    const targetUrl = 'https://tabunblocker-c220451234df.herokuapp.com/';
+    
+    // Get the iframe element by its id
+    const iframe = document.getElementById("hiddenFrame");
+
+    // Set the source URL of the iframe
+    iframe.src = targetUrl;
+});
